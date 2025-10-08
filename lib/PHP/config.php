ed0cf336e952916e12e686faf223c9cb3b9d9e4b
KoenigOne <mm6dd.mh@gmail.com>
2025-10-08 07:34:18 +0100

    modified config file for deployment

--- lib/PHP/config.php
+++ lib/PHP/config.php
@@ -3,29 +3,30 @@
 
 	// connection details for MySQL database
 
-	$cd_host = "127.0.0.1";
-	$cd_port = 3306;
-	$cd_socket = "";
+	// $cd_host = "127.0.0.1";
+	// $cd_port = 3306;
+	// $cd_socket = "";
 
 	// database name, username and password
 
-	$cd_dbname = "companydirectory";
-	$cd_user = "root";
-	$cd_password = "";
-  
-  $conn = new mysqli($cd_host, $cd_user, $cd_password, $cd_dbname, $cd_port, $cd_socket);
+	// $cd_dbname = "companydirectory";
+	// $cd_user = "root";
+	// $cd_password = "";
 
-  if (mysqli_connect_errno()) {
-    $output['status']['code'] = "300";
-    $output['status']['name'] = "failure";
-    $output['status']['description'] = "database unavailable";
-    $output['status']['returnedIn'] = (microtime(true) - $executionStartTime) / 1000 . " ms";
-    $output['data'] = [];
+  $cd_host = "sql109.infinityfree.com"; 
+  $cd_user = "if0_40111963";
+  $cd_password = "9lWW0maCSdwZx";
+  $cd_dbname = "if0_40111963_CompanyDirectory";
 
-    mysqli_close($conn);
+  $conn = new mysqli($cd_host, $cd_user, $cd_password, $cd_dbname);
 
-    echo json_encode($output);
-
-    exit;
+  if ($conn->connect_error) {
+    die(json_encode([
+      "status" => [
+      "code" => 300,
+      "name" => "failure",
+      "description" => "database unavailable: " . $conn->connect_error],
+      "data" => []
+    ]));
   }
 ?>
